433f34ccbc639c1a786432cc6e27772c421940ea
matsem <matej.semancik@gmail.com>
2017-11-18 19:41:11 +0100

    add app dependencies to separate module

--- app/src/main/java/wtf/matsem/gifomat/di/koin_modules.kt
+++ app/src/main/java/wtf/matsem/gifomat/di/koin_modules.kt
@@ -9,17 +9,21 @@ import wtf.matsem.gifomat.tool.camera.GifomatCamera
 import wtf.matsem.gifomat.tool.camera.ImageProcessor
 import wtf.matsem.gifomat.ui.main.MainPresenter
 
-fun getAppModules() = listOf(MainModule())
+fun getAppModules() = listOf(MainModule(), AppModule())
 
 class MainModule : AndroidModule() {
 	override fun context() = applicationContext {
 		context(name = "MainActivity") {
 			provide { MainPresenter(get(), get(), get()) }
 			provide { GifomatCamera(get()) }
-			provide { PeripheralManager() }
 			provide { ImageProcessor() }
 		}
+	}
+}
 
+class AppModule : AndroidModule() {
+	override fun context() = applicationContext {
+		provide { PeripheralManager() }
 		provide { GifomatStore() }
 		provide { applicationContext.getSystemService(Context.CAMERA_SERVICE) } bind CameraManager::class
 	}
